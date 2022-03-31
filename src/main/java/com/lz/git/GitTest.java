9131175d6f5f91c1923895d075e0fdd936191096
liuze <liuze1110@163.com>
2022-03-31 15:46:47 +0800

    second commit

--- src/main/java/com/lz/git/GitTest.java
+++ src/main/java/com/lz/git/GitTest.java
@@ -3,8 +3,6 @@ package com.lz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
-
-
-
 }
